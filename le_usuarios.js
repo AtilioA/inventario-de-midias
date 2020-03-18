aca0bb96d7d9adaa4fb8131a508013f4b00074fc
Philipe A. Marques <lipe.aguiar.marques@gmail.com>
2020-03-18 14:20:02 -0300

    le_usuarios.js adicionado a formataçao esta em forma de object js mas pode mudar dependendo da necessidade

---
+++ le_usuarios.js
@@ -0,0 +1,29 @@
+// Módulo File System para leitura de arquivos
+const fs = require('fs');
+
+function leArquivoUsuarios(caminhoArquivoUsuario) {
+    // Cria string ao ler arquivo CSV local
+    var usuariosCSV = fs.readFileSync(caminhoArquivoUsuario, 'utf8');
+
+    // Separa string do csv em quebras de linha
+    usuariosCSV = usuariosCSV.split("\n");
+
+    // Remove primeiro (header do csv) e último elemento (\n)
+    usuariosCSV.shift();
+    usuariosCSV.pop();
+
+    // usuariosCSV agora possui apenas os usuários a serem cadastrados
+    var usuarios = [];
+    for (usuario in usuariosCSV){
+        var separalinha = usuariosCSV[usuario].split(';');
+        usuarios.push({
+            id: separalinha[0],
+            tipo: separalinha[1],
+            nome: separalinha[2]
+        });
+    }
+    console.log(usuarios);
+}
+
+const caminhoArquivoUsuario = 'entradas/usuarios.csv';
+leArquivoUsuarios(caminhoArquivoUsuario);
